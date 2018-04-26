d0e490c30ee7f20cb4835df91c2af4164e252c46
michal sokol <sokolmichal.ms@gmail.com>
2018-04-26 18:50:30 +0200

    first from intelij po zmianach

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("do wyslania");
+        System.out.println("nastepny");
     }
 }
